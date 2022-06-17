178914110ac38fda8ae14175c0312fc79229a582
Amine <saihiamine569@gmail.com>
2022-06-17 17:08:08 +0100

    changes

--- post.py
+++ post.py
@@ -8,7 +8,6 @@ from firebase_admin import firestore
 def send_data(data):
     cred = credentials.Certificate("firebase_admin.json")
     firebase_admin.initialize_app(cred)
-    #firebase_admin.initialize_app(cred, { "databaseURL": "https://dynamic-return-342300-default-rtdb.firebaseio.com/"})
     db = firestore.client()
     db.collection('test').add(data)
     print('sent data')
